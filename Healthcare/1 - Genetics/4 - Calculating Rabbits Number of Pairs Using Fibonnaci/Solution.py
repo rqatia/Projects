235121e124dce5428c5fe073004e91a7bb43c5fe
Abdulraouf Atia <ralphatia@outlook.com>
2020-12-23 18:29:54 +0000

    Healthcare Project Practice

--- Healthcare/1 - Genetics/4 - Calculating Rabbits Number of Pairs Using Fibonnaci/Solution.py
+++
@@ -1,9 +0,0 @@
-n = 35                      
-k = 5                      
-big = 1                    
-small = 1                  
-for months in range(1,n-1):
-      bigger = big + small*k   
-      small = big              
-      big = bigger             
-print(big)   
